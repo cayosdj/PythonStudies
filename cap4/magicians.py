e8e4bf7e8f36c238c0fb018c500eae284f507c1d
cayosdj <cayosdj@gmail.com>
2020-01-21 14:41:18 -0300

    diretório para os exercícios do capítulo 4

---
+++ cap4/magicians.py
@@ -0,0 +1,8 @@
+#21/01/2020 - Cap 4
+#Magicians Code List
+magicians =['carl jung', 'helena blavatsky', 'aleister crowley', 'hermes trimegisto', 'liz greene', 'jaques de molay'];
+print("Lista dos Magos: ")
+for magician in magicians:
+	print(magician)
+
+print("'For every magician in the list of magicians, print the magician’s name.'")
