e9c0ff9595329e82d86b10798f721d245e007517
cayosdj <cayosdj@gmail.com>
2020-01-23 13:21:26 -0300

    mais exercícios

---
+++ cap4/oddnumbers.py
@@ -0,0 +1,17 @@
+#23/01/2020
+#4-6 Odd Numbers
+print("ODD NUMBERS CODE!")
+oddlist = []
+for odd in range(1,21,2):
+	print(odd)
+	oddlist.append(odd)
+print("Números Ímpares até 20:\n" + str(oddlist))
+
+#4-6 Threes
+
+threeslist =[]
+print("\nMÚLTIPLOS DE 3!!")
+for three in range(3,31,3):
+	print(three)
+	threeslist.append(three)
+print("Números múltiplos de 3 até 30:\n" + str(threeslist))
