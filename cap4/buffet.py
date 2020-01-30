ba7313785ebd32ca5fd84dfc5022c987712b2ef4
cayosdj <cayosdj@gmail.com>
2020-01-30 00:25:02 -0300

    mais exercícios

---
+++ cap4/buffet.py
@@ -0,0 +1,11 @@
+#29/01/2020
+#Buffet tuple code
+foods = ('omelete', 'farofa', 'batata-frita', 'lasanha', 'churrasco')
+print("Pratos da casa:")
+for food in foods:
+	print(food)
+
+print("\nMudança de cardápio:")
+foods = ('ovo-frito', 'canellone', 'batata-frita', 'lasanha', 'churrasco')
+for food in foods:
+	print(food)
